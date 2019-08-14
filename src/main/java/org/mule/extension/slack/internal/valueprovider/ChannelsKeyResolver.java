58ffd6d48a7adaaf18d9140a42aa10c8f396c799
Esteban Wasinger Espro <estebanwasinger@gmail.com>
2019-08-14 18:16:34 -0300

    Fix Channels Key Resolver

--- src/main/java/org/mule/extension/slack/internal/valueprovider/ChannelsKeyResolver.java
+++ src/main/java/org/mule/extension/slack/internal/valueprovider/ChannelsKeyResolver.java
@@ -3,12 +3,14 @@ package org.mule.extension.slack.internal.valueprovider;
 import org.mule.extension.slack.internal.connection.SlackConnection;
 import org.mule.runtime.api.value.Value;
 import org.mule.runtime.core.api.el.ExpressionManager;
+import org.mule.runtime.core.api.util.func.CheckedSupplier;
 import org.mule.runtime.extension.api.annotation.param.Connection;
 import org.mule.runtime.extension.api.values.ValueProvider;
 import org.mule.runtime.extension.api.values.ValueResolvingException;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Supplier;
 
 import javax.inject.Inject;
 
@@ -25,14 +27,18 @@ public class ChannelsKeyResolver implements ValueProvider {
     public Set<Value> resolve() throws ValueResolvingException {
         Set<Value> allChannels = new HashSet<>();
 
-        Set<Value> channels = new ChannelsValueProvider(expressionManager, slackConnection).resolve();
-        Set<Value> groups = new GroupsValueProvider(expressionManager, slackConnection).resolve();
-        Set<Value> ims = new UsersValueProvider(expressionManager, slackConnection).resolve();
-
-        allChannels.addAll(channels);
-        allChannels.addAll(groups);
-        allChannels.addAll(ims);
+        addAllQuitely(allChannels, () -> new ChannelsValueProvider(expressionManager, slackConnection).resolve());
+        addAllQuitely(allChannels, () -> new GroupsValueProvider(expressionManager, slackConnection).resolve());
+        addAllQuitely(allChannels, () -> new UsersValueProvider(expressionManager, slackConnection).resolve());
 
         return allChannels;
+    }
+
+    public void addAllQuitely(Set<Value> allValues, CheckedSupplier<Set<Value>> valueSupplier) {
+        try {
+            allValues.addAll(valueSupplier.get());
+        } catch (Throwable t) {
+            //
+        }
     }
 }
